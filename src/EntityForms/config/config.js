b58b3c35f706541c22c913ea92fb1ff85295ce88
Dvora Riterman <dvorasirota12@gmail.com>
2023-06-21 08:43:28 +0300

    Delete src/EntityForms directory

--- src/EntityForms/config/config.js
+++
@@ -1,9 +0,0 @@
-export const formInitialValues = {
-	input: '',
-	select: { label: '', value: '' },
-	filePicker: {},
-	switch: true,
-	constraints_table: [],
-	simpleDocsPicker: [],
-	timePicker: new Date(),
-};
