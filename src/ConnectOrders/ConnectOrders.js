026389f1ecc9dceb130cb84e24d8c6d2a679e4d3
Dvora Riterman <dvorasirota12@gmail.com>
2023-06-21 08:39:06 +0300

    Delete src/ConnectOrders directory

--- src/ConnectOrders/ConnectOrders.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './ConnectOrders.scss'
-
-function ConnectOrders({height, top, right}) {
-  return <div className='connect-line' 
-    style={{height:height, top:top, right:right}}></div>;
-}
-
-export default ConnectOrders;
