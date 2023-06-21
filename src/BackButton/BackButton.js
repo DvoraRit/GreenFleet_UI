1f765739a4166e94a25e12c0da58edcd218b526f
Dvora Riterman <dvorasirota12@gmail.com>
2023-06-21 08:38:29 +0300

    Delete src/BackButton directory

--- src/BackButton/BackButton.js
+++
@@ -1,9 +0,0 @@
-const BackButton = ({ children, className, ...rest }) => {
-	return (
-		<button className={`back-button ${className}`} {...rest}>
-			{children}
-		</button>
-	);
-};
-
-export default BackButton;
